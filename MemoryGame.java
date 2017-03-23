0e211825f61ea36859cea47689ee32262e2a15fb
taylorealand <taylorealand144@pointloma.edu>
2017-03-23 16:39:05 -0700

    Update MemoryGame.java

--- MemoryGame.java
+++ MemoryGame.java
@@ -8,13 +8,36 @@ public class MemoryGame extends JFrame
    private Container contents;
    private JLabel Level, Score, Timer;
    private JButton Start;
-   private JPanel scorePanel, gamePanel, scorePanel, timerstartPanel;
+   private JPanel scorePanel, gamePanel, levelPanel, timerstartPanel;
    public MemoryGame()
    {
       super("Memory Game");
       contents=getContentPane();
       contents.setLayout(new BorderLayout());
-      //HELLO
+      JButton Start = new JButton("Start");
+      JLabel Timer = new JLabel("Placeholder");
+      JPanel timerstartPanel = new JPanel(new GridLayout(2,1));
+      timerstartPanel.add(Timer);
+      timerstartPanel.add(Start);
+      JLabel level=new JLabel("Level: PLACEHOLDER");
+      JPanel levelPanel = new JPanel();
+      levelPanel.add(level);
+      JLabel score = new JLabel("Score: PLACEHOLDER");
+      JPanel scorePanel=new JPanel();
+      scorePanel.add(score);
+      
+      contents.add(scorePanel,BorderLayout.SOUTH);
+      contents.add(levelPanel,BorderLayout.NORTH);
+      contents.add(timerstartPanel,BorderLayout.EAST);
+      setSize(450,150);
+      setVisible(true);
+      
+   }
+   
+   public static void main(String[]args)
+   {
+      MemoryGame sm=new MemoryGame();
+      sm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
    }
 
 }
